2565c3ffc23a61465942afb847d889a91518f21e
Exudias <editoo31@gmail.com>
2024-06-22 15:00:08 +0300

    Implement tested game board class

---
+++ scripts/gameboard.js
@@ -0,0 +1,97 @@
+const Ship = require("./ship");
+
+class Gameboard
+{
+    constructor()
+    {
+        const GRID_SIZE = 10;
+
+        this.ships = [];
+        this.grid = this._createGrid(GRID_SIZE);
+    }
+
+    place(ship, coords, vertical)
+    {
+        // Check if ship already on board
+        if (this.ships.includes(ship)) return false;
+
+        let gridSize = this.grid.length;
+
+        let startX = coords[1];
+        let startY = coords[0];
+        let endX = startX + (ship.length - 1) * !vertical;
+        let endY = startY + (ship.length - 1) * vertical;
+
+        // Check if OOB
+        if (endX >= gridSize || endY >= gridSize) return false;
+
+        // Check if overlaps other ships
+        for (let i = 0; i < ship.length; i++)
+        {
+            let posY = startY + (i * vertical);
+            let posX = startX + (i * !vertical);
+            if (this.grid[posY][posX] instanceof Ship)
+            {
+                return false;
+            }
+        }
+
+        this.ships.push(ship);
+
+        for (let i = 0; i < ship.length; i++)
+        {
+            let posY = startY + (i * vertical);
+            let posX = startX + (i * !vertical);
+            this.grid[posY][posX] = ship;
+        }
+
+        return true;
+    }
+
+    receiveAttack(coords)
+    {
+        let attackX = coords[1];
+        let attackY = coords[0];
+
+        let hitSquare = this.grid[attackY][attackX];
+        if (hitSquare === "H" || hitSquare === "X") return null;
+
+        if (hitSquare instanceof Ship)
+        {
+            hitSquare.hit();
+            this.grid[attackY][attackX] = "H"
+            return true;
+        }
+
+        this.grid[attackY][attackX] = "X";
+        return false;
+    }
+
+    allShipsSunk()
+    {
+        for (let i = 0; i < this.ships.length; i++)
+        {
+            if (!this.ships[i].isSunk()) return false;
+        }
+
+        return true;
+    }
+
+    _createGrid(size)
+    {
+        let grid = [];
+        for (let i = 0; i < size; i++)
+        {
+            let row = [];
+            for (let j = 0; j < size; j++)
+            {
+                row.push(null);
+            }
+            grid.push(row);
+        }
+
+        return grid;
+    }
+}
+
+module.exports = Gameboard;
---
+++ scripts/gameboard.test.js
@@ -0,0 +1,138 @@
+const Gameboard = require("./gameboard");
+const Ship = require("./ship");
+
+it("Gameboard should place ships into an array", () => {
+    const gameboard = new Gameboard();
+    gameboard.place(new Ship(3), [0, 0], false);
+
+    expect(gameboard.ships.length).toBe(1);
+});
+
+it("Gameboard should place ships horizontally at coordinates and store them", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(3);
+    gameboard.place(ship, [1, 1], false);
+
+    expect(gameboard.grid[1][1]).toBe(ship);
+    expect(gameboard.grid[1][2]).toBe(ship);
+    expect(gameboard.grid[1][3]).toBe(ship);
+});
+
+it("Gameboard should place ships vertically at coordinates and store them", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(3);
+    gameboard.place(ship, [1, 1], true);
+
+    expect(gameboard.grid[1][1]).toBe(ship);
+    expect(gameboard.grid[2][1]).toBe(ship);
+    expect(gameboard.grid[3][1]).toBe(ship);
+});
+
+it("Gameboard should reject horizontal OOB placements", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(2);
+    expect(gameboard.place(ship, [9, 9], false)).toBe(false);
+
+    expect(gameboard.ships.length).toBe(0);
+    expect(gameboard.grid[9][9]).toBe(null);
+});
+
+it("Gameboard should reject vertical OOB placements", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(2);
+    expect(gameboard.place(ship, [9, 9], true)).toBe(false);
+
+    expect(gameboard.ships.length).toBe(0);
+    expect(gameboard.grid[9][9]).toBe(null);
+});
+
+it("Gameboard should allow corner placements (bottom-right)", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(1);
+    expect(gameboard.place(ship, [9, 9], true)).toBe(true);
+
+    expect(gameboard.ships.length).toBe(1);
+    expect(gameboard.grid[9][9]).toBe(ship);
+});
+
+it("Gameboard should allow corner placements (top-left)", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(1);
+    expect(gameboard.place(ship, [0, 0], true)).toBe(true);
+
+    expect(gameboard.ships.length).toBe(1);
+    expect(gameboard.grid[0][0]).toBe(ship);
+});
+
+it("Gameboard should be able to handle attacks properly - hit ship/don't", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(2);
+    gameboard.place(ship, [2, 3], false);
+
+    const spyHit = jest.spyOn(ship, "hit");
+
+    expect(spyHit).toHaveBeenCalledTimes(0);
+
+    expect(gameboard.receiveAttack([2, 3])).toBe(true);
+    expect(spyHit).toHaveBeenCalledTimes(1);
+
+    expect(gameboard.receiveAttack([3, 5])).toBe(false);
+    expect(spyHit).toHaveBeenCalledTimes(1);
+
+    expect(gameboard.receiveAttack([3, 5])).toBe(null);
+    expect(spyHit).toHaveBeenCalledTimes(1);
+})
+
+it("Gameboard should be able to record attacks properly", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(2);
+    gameboard.place(ship, [2, 3], false);
+
+    gameboard.receiveAttack([2, 3]);
+    expect(gameboard.grid[2][3]).toBe("H");
+
+    gameboard.receiveAttack([3, 5]);
+    expect(gameboard.grid[3][5]).toBe("X");
+
+    gameboard.receiveAttack([3, 5]);
+    expect(gameboard.grid[3][5]).toBe("X");
+});
+
+it("Gameboard should report if all ships are sunk", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(2);
+    gameboard.place(ship, [2, 3], false);
+
+    expect(gameboard.allShipsSunk()).toBe(false);
+
+    gameboard.receiveAttack([2, 3]);
+
+    expect(gameboard.allShipsSunk()).toBe(false);
+
+    gameboard.receiveAttack([2, 4]);
+
+    expect(gameboard.allShipsSunk()).toBe(true);
+});
+
+it("Gameboard should not allow the same ship to be placed twice", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(2);
+
+    expect(gameboard.ships.length).toBe(0);
+    expect(gameboard.place(ship, [2, 3], false)).toBe(true);
+    expect(gameboard.ships.length).toBe(1);
+    expect(gameboard.place(ship, [7, 4], false)).toBe(false);
+    expect(gameboard.ships.length).toBe(1);
+});
+
+it("Gameboard should not allow overlapping ships to be placed", () => {
+    const gameboard = new Gameboard();
+    const ship1 = new Ship(2);
+    const ship2 = new Ship(2);
+
+    expect(gameboard.ships.length).toBe(0);
+    expect(gameboard.place(ship1, [2, 3], false)).toBe(true);
+    expect(gameboard.ships.length).toBe(1);
+    expect(gameboard.place(ship2, [2, 4], false)).toBe(false);
+    expect(gameboard.ships.length).toBe(1);
+});
